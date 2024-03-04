9cdf38c768ab6c91227e3b63033cd57b7cc4a8e7
底潮 <dearchar@qq.com>
2024-03-04 11:41:02 +0800

    获取内存

--- androidProfiler/src/main/java/com/dc/androidprofiler/JNI.java
+++ androidProfiler/src/main/java/com/dc/androidprofiler/JNI.java
@@ -4,7 +4,5 @@ public class JNI {
     static {
         System.loadLibrary("Memory");
     }
-    public native String sayHello();
-
     public static native MallInfo getMallinfo();
 }
--- androidProfiler/src/main/jni/Memory.c
+++ androidProfiler/src/main/jni/Memory.c
@@ -2,24 +2,14 @@
 #include <stdlib.h>
 #include <jni.h>
 
-JNIEXPORT jstring JNICALL
-Java_com_dc_safearea_JNI_sayHello(JNIEnv *env, jobject thiz) {
-    printf("1");
-}
-
 jobject JNICALL
-Java_com_dc_safearea_JNI_getMallinfo(JNIEnv *env, jobject thiz) {
-printf("1.1");
-struct mallinfo info = mallinfo();
-long  nativeHeapSize =info.usmblks;
-long  nativeHeapAlloc =info.uordblks;
-long  nativeHeapFree =info.fordblks;
-printf("1.2");
-jclass mallInfoClass = (*env)->FindClass(env, "com/dc/memoryprofiler/MallInfo");
-printf("1.3");
-jmethodID constructor = (*env)->GetMethodID(env, mallInfoClass, "<init>", "(IIIIII)V");
-printf("1.4");
-jobject mallInfoObj = (*env)->NewObject(env, mallInfoClass, constructor,nativeHeapSize, nativeHeapAlloc, nativeHeapFree);
-printf("1.5");
-return mallInfoObj;
+Java_com_dc_androidprofiler_JNI_getMallinfo(JNIEnv *env, jobject thiz) {
+    struct mallinfo info = mallinfo();
+    jlong  nativeHeapSize = (jlong)info.usmblks;
+    jlong  nativeHeapAlloc = (jlong)info.uordblks;
+    jlong  nativeHeapFree = (jlong)info.fordblks;
+    jclass mallInfoClass = (*env)->FindClass(env, "com/dc/androidprofiler/MallInfo");
+    jmethodID constructor = (*env)->GetMethodID(env, mallInfoClass, "<init>", "(JJJ)V");
+    jobject mallInfoObj = (*env)->NewObject(env, mallInfoClass, constructor,nativeHeapSize, nativeHeapAlloc, nativeHeapFree);
+    return mallInfoObj;
 }
